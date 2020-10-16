a194f4894d1d57a29fff9800ca65011f65ac0951
Juraj Nyíri <jnyiri@sk.ibm.com>
2020-10-16 23:26:36 +0200

    Add: Ability to change username and password via UI

--- custom_components/tapo_control/config_flow.py
+++ custom_components/tapo_control/config_flow.py
@@ -1,4 +1,3 @@
-from pytapo import Tapo
 from homeassistant import config_entries
 from homeassistant.const import (
     CONF_IP_ADDRESS,
@@ -13,19 +12,27 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DEVICE_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_IP_ADDRESS): str,
-        vol.Required(CONF_USERNAME): str,
-        vol.Required(CONF_PASSWORD): str
-    }
-)
+@config_entries.HANDLERS.register(DOMAIN)
+class FlowHandler(config_entries.ConfigFlow):
+    """Handle a config flow."""
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return TapoOptionsFlowHandler(config_entry)
+
     async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        return await self.async_step_auth()
+
+    async def async_step_auth(self, user_input=None):
+        """Confirm the setup."""
         errors = {}
+        host = ""
+        username = ""
+        password = ""
         if user_input is not None:
             try:
                 host = user_input[CONF_IP_ADDRESS]
@@ -48,5 +55,60 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     _LOGGER.error(e)
 
         return self.async_show_form(
-            step_id="user", data_schema=DEVICE_SCHEMA, errors=errors
+            step_id="auth", 
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_IP_ADDRESS, description={"suggested_value": host}): str,
+                    vol.Required(CONF_USERNAME, description={"suggested_value": username}): str,
+                    vol.Required(CONF_PASSWORD, description={"suggested_value": password}): str
+                }
+            ), 
+            errors=errors
+        )
+
+class TapoOptionsFlowHandler(config_entries.OptionsFlow):
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+        self.options = dict(config_entry.options)
+
+    async def async_step_init(self, user_input=None):
+        """Manage the Tapo options."""
+        return await self.async_step_auth()
+
+    async def async_step_auth(self, user_input=None):
+        """Manage the Tapo options."""
+        errors = {}
+        username = self.config_entry.data[CONF_USERNAME]
+        password = self.config_entry.data[CONF_PASSWORD]
+        if user_input is not None:
+            try:
+                host = self.config_entry.data['ip_address']
+                username = user_input[CONF_USERNAME]
+                password = user_input[CONF_PASSWORD]
+
+                await self.hass.async_add_executor_job(registerController, host, username, password)
+
+                self.hass.config_entries.async_update_entry(
+                    self.config_entry, data={CONF_IP_ADDRESS: host, CONF_USERNAME: username, CONF_PASSWORD: password,},
+                )
+                return self.async_create_entry(title="", data=None)
+            except Exception as e:
+                if("Failed to establish a new connection" in str(e)):
+                    errors["base"] = "connection_failed"
+                elif(str(e) == "Invalid authentication data."):
+                    errors["base"] = "invalid_auth"
+                else:
+                    errors["base"] = "unknown"
+                    _LOGGER.error(e)
+
+        return self.async_show_form(
+            step_id="auth",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME, description={"suggested_value": username}): str,
+                    vol.Required(CONF_PASSWORD, description={"suggested_value": password}): str
+                }
+            ),
+            errors=errors,
         )
