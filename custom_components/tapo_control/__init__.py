c89261f931a4fcc9604fd8e88fb2bc05ed096c42
Juraj Nyíri <jnyiri@sk.ibm.com>
2022-12-21 11:53:10 +0100

    Fix: Camera entities not created

--- custom_components/tapo_control/__init__.py
+++ custom_components/tapo_control/__init__.py
@@ -292,14 +292,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, "button")
         )
-        if camData["childDevices"] is None:
+        if camData["childDevices"] is False or camData["childDevices"] is None:
             hass.async_create_task(
                 hass.config_entries.async_forward_entry_setup(entry, "camera")
             )
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, "light")
         )
-
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, "number")
         )
@@ -309,7 +308,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, "switch")
         )
-
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, "update")
         )
