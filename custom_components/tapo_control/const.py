12677bd81d1ffd90561b12c4375762781b24965b
Juraj Nyíri <juraj.nyiri@gmail.com>
2024-03-11 12:46:11 +0100

    Fix: Edge case where downloading media would fail

--- custom_components/tapo_control/const.py
+++ custom_components/tapo_control/const.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 
 from homeassistant.helpers import config_validation as cv
 
-PYTAPO_REQUIRED_VERSION = "3.3.18"
+PYTAPO_REQUIRED_VERSION = "3.3.19"
 DOMAIN = "tapo_control"
 BRAND = "TP-Link"
 ALARM_MODE = "alarm_mode"
